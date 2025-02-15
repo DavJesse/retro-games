dc4bea1ec122648d1090bd709c1d2e0c225d2505
davodhiambo <jesseomolo@yahoo.com>
2025-02-15 19:01:40 +0300

    files: add js to load content of brick breaker game.

---
+++ brick_breaker/js/load-brick-breaker.js
@@ -0,0 +1,19 @@
+export function loadBrickBreaker() {
+    const gameContainer = document.getElementById("game-container");
+
+    // Create paddle
+    const paddle = document.createElement("div");
+    paddle.id = "paddle";
+    gameContainer.appendChild(paddle);
+
+     // Create bricks
+     const bricks = document.createElement("div");
+     bricks.id = "bricks";
+     gameContainer.appendChild(bricks);
+
+      // Create paddle
+    const script = document.createElement("script");
+    script.src = "brick_breaker/js/brick_breaker.js";
+    script.defer = true;
+    document.body.appendChild(script);
+}
