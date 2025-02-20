e8ca1e287ea5995f48c6c37af1cb51af1c87c2c6
tesiaka <teddysiakaogola@gmail.com>
2025-02-20 08:16:53 +0300

    chore:rename all functions and variable to  meaningful names

--- brick_breaker/js/brickmaker.js
+++ brick_breaker/js/brickmaker.js
@@ -1,50 +1,66 @@
-var Normalbricks = ["brick red", "brick orange", "brick yellow", "brick green", "brick blue"]
-var Hardbrick = ["brick steel cracked", "brick iron", "brick titanium", "brick platinum", "brick steel", "brick chrome", "brick metallic"]
-var lifebrick = ["brick life-up"]
-var crackedbrick = ["brick yellow cracked", "brick red svg-cracked", "brick yellow svg-cracked", "brick green svg-cracked", "brick orange svg-cracked", "brick blue svg-cracked"]
-var explosivebrick = ["brick explosive"]
+var normalBricks = ["brick red", "brick orange", "brick yellow", "brick green", "brick blue"];
+var hardBricks = ["brick steel cracked", "brick iron", "brick titanium", "brick platinum", "brick steel", "brick chrome", "brick metallic"];
+var lifeBricks = ["brick life-up"];
+var crackedBricks = ["brick yellow cracked", "brick red svg-cracked", "brick yellow svg-cracked", "brick green svg-cracked", "brick orange svg-cracked", "brick blue svg-cracked"];
+var explosiveBricks = ["brick explosive"];
 
-var getbrick = (bricks) => {
-   let x = Math.floor(Math.random() * bricks.length)
-   console.log(typeof bricks[x])
-   return bricks[x]
-}
-export function Makebrick(level = 1) {
-   var brickcontainer = document.createElement("div")
-   brickcontainer.id = "brick-container"
-   document.getElementById("game-container").appendChild(brickcontainer)
-
-   if (level == 1) {
-       level1(brickcontainer)
-   }
-}
-function level1(brickcontainer) {
-   let bricks = [...Normalbricks, crackedbrick, lifebrick]
-   
-   let NumberOfBricks=1
-   let numberOfbricklife=0;
-    while (NumberOfBricks<=35) {
-      let bricktype=getbrick(bricks)
-      if (bricktype == lifebrick[0]){
-         numberOfbricklife++
-         console.log(typeof bricktype)
-      }
-       if (numberOfbricklife>3 && bricktype === lifebrick[0] ){
-              continue
-       }else{
-         let brick = document.createElement("div")
-         brick.setAttribute("class",bricktype)
-         brickcontainer.appendChild(brick)
-        NumberOfBricks++
-       }
-
-    }
-
-   return bricks
-
-}
 class Brick {
-   constructor(parameters) {
-
+   constructor(type, top, bottom, left, right) {
+      this.type = type;
+      this.top = top;
+      this.bottom = bottom;
+      this.left = left;
+      this.right = right;
    }
+}
+
+export var brickPositions = [];
+
+var getRandomBrick = (brickList) => {
+   let randomIndex = Math.floor(Math.random() * brickList.length);
+   return brickList[randomIndex];
+};
+
+export function generateBricks(level = 1) {
+   var gameContainer = document.getElementById("game-container");
+   if (!gameContainer) {
+      console.error("Game container not found!");
+      return;
+   }
+
+   var brickContainer = document.createElement("div");
+   brickContainer.id = "brick-container";
+   gameContainer.appendChild(brickContainer);
+
+   if (level === 1) {
+      createLevelOneBricks(brickContainer);
+   }
+}
+
+function createLevelOneBricks(brickContainer) {
+   let brickTypes = [...normalBricks, ...crackedBricks, lifeBricks[0]];
+   let totalBricks = 0;
+   let lifeBrickCount = 0;
+   const maxLifeBricks = 3;
+   const maxBricks = 35;
+
+   while (totalBricks < maxBricks) {
+      let brickType = getRandomBrick(brickTypes);
+
+      if (brickType === lifeBricks[0]) {
+         if (lifeBrickCount >= maxLifeBricks) continue;
+         lifeBrickCount++;
+      }
+
+      let brickElement = document.createElement("div");
+      brickElement.setAttribute("class", brickType);
+      brickContainer.appendChild(brickElement);
+
+      let brickDimensions = brickElement.getBoundingClientRect();
+      brickPositions.push(new Brick(brickType, brickDimensions.top, brickDimensions.bottom, brickDimensions.left, brickDimensions.right));
+
+      totalBricks++;
+   }
+
+   return brickTypes;
 }
